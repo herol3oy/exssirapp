c1a6427750d7422fb7cbc24d3fec24efd2ffb8fd
Hamed Sedighi <hamedsedighi@gmail.com>
2022-03-26 23:00:21 +0100

    :lipstick: Add input label helper

--- components/poemFirstPart.tsx
+++ components/poemFirstPart.tsx
@@ -3,7 +3,7 @@ import {
   beytFirstPartAnswer,
   beytFirstPartWords,
 } from '@/utils/createPoemVariables'
-import { Flex, Input, Text } from '@chakra-ui/react'
+import { Flex, FormControl, FormLabel, Input, Text } from '@chakra-ui/react'
 import { PoemFirstPartType } from 'model/poem-first-part'
 import { ChangeEvent } from 'react'
 import 'react-simple-keyboard/build/css/index.css'
@@ -17,25 +17,40 @@ const PoemFirstPart = ({
     <Flex alignItems={['flex-end', 'flex-start']}>
       {beytFirstPartWords?.map((word: string) =>
         word === beytFirstPartAnswer ? (
-          <Input
-            key={word}
-            borderBottomColor={isBeytFirstPartAnswerCorrect ? 'green.300' : ''}
-            textAlign='center'
-            fontWeight={900}
-            variant='flushed'
-            disabled
-            placeholder={answerHintPlaceholder}
-            color={isBeytFirstPartAnswerCorrect ? 'green.300' : 'gray'}
-            value={userInputAnswer}
-            onChange={(e: ChangeEvent<HTMLInputElement>): void =>
-              userInputAnswerSet(e.target.value)
-            }
-            fontSize={['1.2rem', '2rem', '2.2rem', '2.5rem']}
-            maxW='250px'
-            mr={2}
-            p={6}
-            maxLength={5}
-          />
+          <FormControl>
+            <Input
+              key={word}
+              borderBottomColor={
+                isBeytFirstPartAnswerCorrect ? 'green.300' : ''
+              }
+              textAlign='center'
+              fontWeight={900}
+              variant='flushed'
+              disabled
+              placeholder={answerHintPlaceholder}
+              color={isBeytFirstPartAnswerCorrect ? 'green.300' : 'gray'}
+              value={userInputAnswer}
+              onChange={(e: ChangeEvent<HTMLInputElement>): void =>
+                userInputAnswerSet(e.target.value)
+              }
+              fontSize={['1.2rem', '2rem', '2.2rem', '2.5rem']}
+              maxW='250px'
+              mr={2}
+              p={6}
+              maxLength={5}
+            />
+            <FormLabel
+              htmlFor='text'
+              position='absolute'
+              top={0}
+              right='15px'
+              transition='opacity 0.3s ease'
+              fontSize='xs'
+              opacity={userInputAnswer ? 1 : 0}
+            >
+              {answerHintPlaceholder}
+            </FormLabel>
+          </FormControl>
         ) : (
           <Text mr={2} color='GrayText' key={word}>
             {word}
