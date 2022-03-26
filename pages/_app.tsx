87e07895692debe165f522be5cba43301d00395f
Hamed Sedighi <hamedsedighi@gmail.com>
2022-03-26 20:28:19 +0100

    :fire: Add pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,23 @@ import { RtlProvider } from '@/utils/rtl-provider'
 import customTheme from '@/utils/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
       <RtlProvider>
         <Layout>
-          <Component {...pageProps} />
+          <>
+            <Head>
+              <title>اکسیر | هر روز سخنی از عشق</title>
+              <meta
+                name='viewport'
+                content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+              />
+            </Head>
+            <Component {...pageProps} />
+          </>
         </Layout>
       </RtlProvider>
     </ChakraProvider>
