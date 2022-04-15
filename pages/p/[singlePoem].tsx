2e1ae0ed910910d6c6d200f1105a769ffa2286ad
Hamed Sedighi <hamedsedighi@gmail.com>
2022-04-15 15:59:26 +0200

    :fire: Add previous poems answer page

---
+++ pages/p/[singlePoem].tsx
@@ -0,0 +1,38 @@
+import { poems } from '@/db/poems'
+import { Poem } from '@/model/Poem'
+import { daysSince } from '@/utils/countDay'
+import { Text } from '@chakra-ui/react'
+
+export default function SinglePoem({ poem }: { poem: Poem }) {
+  return (
+    <Text mr={2} color='GrayText'>
+      <pre dir='ltr'>{JSON.stringify(poem, null, 2)}</pre>
+    </Text>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = poems.map((_, poemIndex) => ({
+    params: { singlePoem: poemIndex.toString() },
+  }))
+
+  return { paths, fallback: true }
+}
+
+export async function getStaticProps({
+  params,
+}: {
+  params: { singlePoem: string }
+}) {
+  if (+params.singlePoem < daysSince) {
+    const poem = poems[+params.singlePoem]
+
+    return {
+      props: { poem },
+    }
+  } else {
+    return {
+      props: { poem: 'no-way' },
+    }
+  }
+}
